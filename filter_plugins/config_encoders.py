4379c84bc59f7a69d4a3f90e8f5846ae677d313e
nervo <nervo@nervo.net>
2020-10-26 00:52:38 +0100

    Ensure list is not empty before checking its first entry in toml encoder (#15)
    Given a sructure like:
    ```
    foo: []
    ```
    the toml encoder failed with an `IndexError: list index out of range` error, due to access tries on first element of an empty list

--- filter_plugins/config_encoders.py
+++ filter_plugins/config_encoders.py
@@ -794,7 +794,7 @@ def encode_toml(
 
                 first = False
                 tn = ''
-            elif isinstance(v, list) and not isinstance(v[0], dict):
+            elif isinstance(v, list) and (not v or not isinstance(v[0], dict)):
                 if tn:
                     if not first:
                         rv += "\n"
@@ -851,7 +851,7 @@ def encode_toml(
                     table_type='table')
 
                 first = False
-            elif isinstance(v, list) and isinstance(v[0], dict):
+            elif isinstance(v, list) and (not v or isinstance(v[0], dict)):
                 # Array of tables
                 tk = k
 
